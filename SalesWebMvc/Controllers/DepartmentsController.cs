1f6f34b4c191243a3c777f6e356f0a7bf61dda3c
Alison <alisson_gamer2000@outlook.com>
2022-12-21 15:04:18 -0300

    add view and controller

---
+++ SalesWebMvc/Controllers/DepartmentsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
+using System.Collections.Generic;
+
+namespace SalesWebMvc.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            //instanciando uma lista de Departments
+
+            List<Department> list = new List<Department>();
+
+            //add na list
+            list.Add(new Department { Id = 1, Name = "Eletronics"});
+            list.Add(new Department { Id = 2, Name = "Fashion"});
+
+            //para enviar essa lista só colocar no method view
+            return View(list);
+        }
+    }
+}
