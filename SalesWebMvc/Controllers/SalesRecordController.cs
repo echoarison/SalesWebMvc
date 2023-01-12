99a676dd91264895de22c1a1f8d5640a2596cf3b
Alison <alisson_gamer2000@outlook.com>
2023-01-12 16:45:03 -0300

    Preparing sales search navigation views

---
+++ SalesWebMvc/Controllers/SalesRecordController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SalesWebMvc.Controllers
+{
+    public class SalesRecordController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SimpleSearch()
+        {
+            return View();
+        }
+
+        public IActionResult GroupingSearch()
+        {
+            return View();
+        }
+    }
+}
